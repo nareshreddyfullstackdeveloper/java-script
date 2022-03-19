9c037f15427b6e55ce5de8872528f980c405a856
nareshreddyfullstackdeveloper <98952876+nareshreddyfullstackdeveloper@users.noreply.github.com>
2022-03-19 11:42:24 +0530

    init

---
+++ four.js
@@ -0,0 +1,8 @@
+let greet1 = (msg)=> {
+    return msg;
+};
+let result = greet1('Good morning');
+console.log(result);
+let greet2 = (msg) =>msg;
+
+console.log(greet2('Good night'));
--- three.js
+++ three.js
@@ -1,5 +1,5 @@
-let add = (a,b,c) =>{
-    return a + b + c+ 200;
+let add = (a,b,c) => {
+    return a + b + c + 400;
 };
-let result = add(20,30,40);
+let result = add(20,30,50);
 console.log(result);
